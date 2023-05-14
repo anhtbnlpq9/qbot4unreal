096318429c1fd0c25a40b70306530e7494256c52
thib <thib@example.net>
2023-05-14 08:10:21 +0000

    Code cleanup

--- Client.java
+++ Client.java
@@ -70,8 +70,8 @@ public class Client implements Runnable {
 
     public void launchCService() {
         
-        Map<String, ServerNode> serverList = protocol.getServerList();
-        Map<String, UserNode> userList = protocol.getUserList();
+        Map<String, ServerNode>   serverList = protocol.getServerList();
+        Map<String, UserNode>       userList = protocol.getUserList();
         Map<String, ChannelNode> channelList = protocol.getChanList();
 
         cservice = new CService(this, protocol, sqliteDb);
