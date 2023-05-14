fc2a9f3767f0e4475688bdb40ee25d1ec7794044
thib <thib@example.net>
2023-05-14 08:09:48 +0000

    Add javadoc in Help class

--- Help.java
+++ Help.java
@@ -2,13 +2,30 @@ import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+* Help class
+* @author me
+*/
 public class Help {
+
+    /**
+     * Constructor
+     */
     public Help() {
+        
     }
 
+    /**
+     * Method to return the content of the file
+     * @param type Help type (levels, commands, ...)
+     * @param command Command name
+     * @return File content
+     */
     public static ArrayList<String> getHelp(String type, String command) {
+
         ArrayList<String> content = new ArrayList<String>();
         String currentLine;
+
         try {
             FileInputStream fis=new FileInputStream("help/" + type + "/" + command + ".txt");       
             Scanner sc=new Scanner(fis);
