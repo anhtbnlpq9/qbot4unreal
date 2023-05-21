1de663dbc75b141336c2d058af847a2f601601ad
thib <thib@example.net>
2023-05-21 19:23:57 +0000

    Forgot to commit the database scheduled tasks class

---
+++ SqliteDbTasks.java
@@ -0,0 +1,27 @@
+
+/**
+ * Threaded class to perform periodic database operations
+ */
+public class SqliteDbTasks implements Runnable {
+
+    public SqliteDb sqliteDb;
+    public Boolean threadRunning = true;
+    
+    public SqliteDbTasks(SqliteDb sqliteDb) {
+        this.sqliteDb = sqliteDb;
+
+    }
+
+    public void run() {
+
+        System.out.println("* Started db cron thread");
+
+        try {
+            while(threadRunning == true) {
+                Thread.sleep(60000); // to put inside conf file
+                sqliteDb.cleanInvalidLoginTokens();
+            }
+        }
+        catch (Exception e) { e.printStackTrace(); }
+    }
+}
