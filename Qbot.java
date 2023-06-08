bb27c3e7377f53b54761fb613fc585e7fa7ea157
thib <thib@example.net>
2023-06-08 18:56:06 +0000

    Add command line arguments handling

--- Qbot.java
+++ Qbot.java
@@ -17,6 +17,24 @@ public class Qbot {
      * @param args no arguments
      */
     public static void main(String[] args) {
+        String configFileName = "";
+        String configFile;
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (arg.equals("-h") == true || arg.equals("--help")) {
+                printUsage();
+                return;
+            } else if (arg.equals("-c") == true || arg.equals("--config")) {
+                configFileName = args[++i];
+            } else {
+                // invalid argument
+                printUsage();
+                return;
+            }
+        }
+
+
         log.log(INFO2, "  ");
         log.log(INFO2, "                      _..---...,\"\"-._     ,/}/)");
         log.log(INFO2, "                   .''        ,      ``..'(/-< ");
@@ -37,9 +55,12 @@ public class Qbot {
         log.log(INFO2, "  | | )(-  \\_\\/  |__)(_)|_  | (_)|   \\__/| )| (-(_|||| \\ \\__(_| ");
         log.log(INFO2, " ");
 
-        log.info("Loading configuration file");
         
-        Config config = new Config(CONFIG_FILE);
+        if (configFileName.isEmpty() == false) configFile = configFileName;
+        else configFile = CONFIG_FILE;
+        
+        log.info("Loading configuration file: " + configFile);
+        Config config = new Config(configFile);
 
         log.info("Opening database");
         SqliteDb sqliteDb = new SqliteDb(config);
@@ -68,5 +89,12 @@ public class Qbot {
         Thread sqliteDbCleanupThread = new Thread(sqliteDbCleanup);
         sqliteDbCleanupThread.start();
 
-    }    
+    }
+
+    private static void printUsage() {
+
+        System.out.println("Usage: java Qbot [-h | --help] [-c file]");
+
+    }
+
 }
