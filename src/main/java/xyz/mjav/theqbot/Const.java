5e09b6feb36d284ee699b1b7bab649ec469019fb
thib <thib@example.net>
2024-06-12 20:19:47 +0000

    Version 1.1.1

--- src/main/java/xyz/mjav/theqbot/Const.java
+++ src/main/java/xyz/mjav/theqbot/Const.java
@@ -10,7 +10,7 @@ public abstract class Const {
     private static Config config = Config.getConfig();
 
     /* Generic constants */
-    public static final String QBOT_VERSION_NUMBER = "1.1";
+    public static final String QBOT_VERSION_NUMBER = "1.1.1";
     public static final String QBOT_VERSION_STRING = String.format("TheQBot4Unreal-%s", QBOT_VERSION_NUMBER);
 
     public static final String UNREAL_PROTOCOL_VERSION = "6100";
