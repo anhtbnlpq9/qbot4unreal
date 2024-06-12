57a97fb5e533232a43e077f7a193ba5bc49a75ba
thib <thib@example.net>
2024-06-12 19:46:40 +0000

    Rename ChanAutoLimit class

--- src/main/java/xyz/mjav/theqbot/ChanAutoLimitTask.java
+++ src/main/java/xyz/mjav/theqbot/ChanAutoLimitTask.java
@@ -3,21 +3,21 @@ package xyz.mjav.theqbot;
 /**
  * Class to implement the channel autolimit user limit auto setting feature periodically
  */
-public class ChanAutoLimit extends ScheduledTask {
+public class ChanAutoLimitTask extends ScheduledTask {
 
     protected void exec() {
         cservice.setAutolimit();
     }
 
-    public static ChanAutoLimit create() {
+    public static ChanAutoLimitTask create() {
         ScheduledTask.classInit();
-        return new ChanAutoLimit();
+        return new ChanAutoLimitTask();
     }
 
     /**
      * Class constructor
      */
-    public ChanAutoLimit() {
+    public ChanAutoLimitTask() {
         super("ChanAutoLimit", config.getCServeAutoLimitFreq(), false);
     }
 
