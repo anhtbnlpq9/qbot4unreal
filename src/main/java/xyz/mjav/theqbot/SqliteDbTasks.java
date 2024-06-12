cc6b68314c2e96be501d82757f18620cf144de3d
thib <thib@example.net>
2024-06-12 20:00:45 +0000

    SqliteDbTask scheduler now uses Scheduler API

--- src/main/java/xyz/mjav/theqbot/SqliteDbTasks.java
+++ src/main/java/xyz/mjav/theqbot/SqliteDbTasks.java
@@ -1,36 +1,24 @@
 package xyz.mjav.theqbot;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 /**
  * Threaded class to perform periodic database operations
  */
-public class SqliteDbTasks implements Runnable {
-
-    private static Logger log = LogManager.getLogger("common-log");
+public class SqliteDbTasks extends ScheduledTask {
 
     private Database sqliteDb;
-    public Boolean isThreadRunning = true;
+
+    protected void exec() {
+        log.debug("SqliteDbTasks::exec: performing database task");
+        sqliteDb.cleanInvalidLoginTokens();
+    }
 
     /**
      * Class constructor
      * @param sqliteDb
      */
     public SqliteDbTasks(Database sqliteDb) {
+        super("SqliteDbTasks", config.getDbSchedFreq(), false);
         this.sqliteDb = sqliteDb;
     }
 
-    public void run() {
-
-        log.info("Started db cron thread");
-
-        try {
-            while(isThreadRunning == true) {
-                Thread.sleep(60000); // to put inside conf file
-                sqliteDb.cleanInvalidLoginTokens();
-            }
-        }
-        catch (Exception e) { log.error("SqliteDbTasks/run: error starting database tasks: ", e); }
-    }
 }
