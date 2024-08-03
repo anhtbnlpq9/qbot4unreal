05dd3efd33eb795978e4b155b815bc13106e4317
thib <thib@example.net>
2024-08-03 10:14:33 +0000

    Preparation for Oper Service

--- src/main/java/xyz/mjav/theqbot/OService.java
+++ src/main/java/xyz/mjav/theqbot/OService.java
@@ -1,12 +1,58 @@
 package xyz.mjav.theqbot;
 
+import static java.util.Map.entry;
+
 import java.time.Instant;
+import java.util.HashMap;
 import java.util.Map;
 
 import java.util.TreeMap;
 
 
 public class OService extends Service {
+
+
+    private static final Map<String, String> CMD_ALWAYS = Map.ofEntries(
+
+    );
+
+    private static final Map<String, String> CMD_NOAUTH = Map.ofEntries(
+
+    );
+
+    private static final Map<String, String> CMD_AUTHED = Map.ofEntries(
+
+    );
+
+    private static final Map<String, String> CMD_STAFF = Map.ofEntries(
+
+    );
+
+    private static final Map<String, String> CMD_OPER = Map.ofEntries(
+
+    );
+
+    private static final Map<String, String> CMD_ADMIN = Map.ofEntries(
+
+    );
+
+    private static final Map<String, String> CMD_DEVGOD = Map.ofEntries(
+
+    );
+
+    /** List of the commands */
+    private static final Map<String, String> CMD_LIST;
+    static {
+        CMD_LIST = new HashMap<>();
+        CMD_LIST.putAll(CMD_ALWAYS);
+        CMD_LIST.putAll(CMD_NOAUTH);
+        CMD_LIST.putAll(CMD_AUTHED);
+        CMD_LIST.putAll(CMD_STAFF);
+        CMD_LIST.putAll(CMD_OPER);
+        CMD_LIST.putAll(CMD_ADMIN);
+        CMD_LIST.putAll(CMD_DEVGOD);
+    };
+
 
     /**
      * Class constructor
@@ -64,20 +110,45 @@ public class OService extends Service {
     }
 
 
-    public void handleMessage(Nick fromNickRaw, String str) throws Exception {
-        Nick fromNick = fromNickRaw;
-        String commandName;
-        String[] strSplit;
+    public void handleMessage(IrcMessage ircMsg) throws Exception {
 
-        try {
-            strSplit = str.split(" ");
-            commandName = strSplit[0].toUpperCase();
-        }
-        catch (IndexOutOfBoundsException e) { sendReply(fromNick, Messages.strErrCommandUnknown); return; }
+        CSCommand osCommand = CSCommand.create(ircMsg);
 
-        switch (commandName) {
-            default:                  sendReply(fromNick, Messages.strErrCommandUnknown); break;
+        switch (osCommand.getCommandName()) {
+
+            case "NETINFO":           oServeNetinfo(osCommand); break;
+            case "VERSION":           oServeVersion(osCommand); break;
+
+            default:                  sendReply(osCommand.getFromNick(), Messages.strErrCommandUnknown); break;
         }
 
+    }
+
+
+    private void oServeHelp(CSCommand csCommand) {
+        /* HELP <command> */
+
+        /** Command name UPPERCASE */
+        String helpCommandName;
+
+        try { helpCommandName = csCommand.getArgs().get(0).toUpperCase(); }
+        catch (IndexOutOfBoundsException e) { /*getShowcommands(csCommand, "showcommandsshort");*/ return; }
+
+        /* Commands does not exist */
+        if (CMD_LIST.containsKey(helpCommandName) == false) {
+            sendReply(csCommand.getFromNick(), String.format(Messages.strErrNoAccess));
+            return;
+        }
+
+       // sendHelp(csCommand.getFromNick(), helpCommandName);
+
+    }
+
+    private void oServeVersion(CSCommand csCommand) {
+        sendReply(csCommand.getFromNick(), Const.QBOT_VERSION_STRING);
+    }
+
+    private void oServeNetinfo(CSCommand csCommand) {
+        sendReply(csCommand.getFromNick(), Const.QBOT_VERSION_STRING);
     }
 }
